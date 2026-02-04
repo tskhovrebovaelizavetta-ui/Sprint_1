792692fcd3a58d6552e78c5d9d29a1692dc4436f
tskhovrebovaelizavetta-ui <tskhovrebovaelizavetta@gmail.com>
2026-02-04 20:39:56 +0300

    first commit

---
+++ task_4.py
@@ -0,0 +1,12 @@
+new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
+completed_tasks = ['task_002', 'task_012', 'task_006']
+completed_tasks.append(new_tasks.pop(4))
+
+print(new_tasks)
+print(completed_tasks)
+
+new_tasks.remove('task_007')
+print(new_tasks)
+
+new_tasks.insert(0, new_tasks.pop(2))
+print(new_tasks)
