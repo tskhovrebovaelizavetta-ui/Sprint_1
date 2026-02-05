aa4f8ceab81bf8a583450333c2771dba501254a9
tskhovrebovaelizavetta-ui <tskhovrebovaelizavetta@gmail.com>
2026-02-05 09:41:20 +0300

    2 commit

--- task_4.py
+++ task_4.py
@@ -1,6 +1,6 @@
 new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
 completed_tasks = ['task_002', 'task_012', 'task_006']
-completed_tasks.append(new_tasks.pop(4))
+completed_tasks.append(new_tasks.pop())
 
 print(new_tasks)
 print(completed_tasks)
@@ -9,4 +9,5 @@ new_tasks.remove('task_007')
 print(new_tasks)
 
 new_tasks.insert(0, new_tasks.pop(2))
-print(new_tasks)
+last_task = new_tasks[0]  # task_015
+print('Задача с повышенным приоритетом (следующая в работе): ' + last_task)
